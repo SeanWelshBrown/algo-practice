073f09ba0146895b5477591ae7af74898fd18c57
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-08-16 11:51:27 -0400

    Added mergeSort algo

---
+++ mergeSort.js
@@ -0,0 +1,48 @@
+function mergeSort(arr) {
+
+  if (arr.length === 1) return arr;
+
+  let midpoint = Math.floor(arr.length / 2);
+
+  let a = arr.slice(0, midpoint);
+  let b = arr.slice(midpoint);
+
+  a = mergeSort(a);
+  b = mergeSort(b);
+
+  return merge(a, b);
+
+}
+
+function merge(a, b) {
+
+  let c = [];
+
+  let i = 0, j = 0;
+  while (i < a.length && j < b.length) {
+    if (a[i] < b[j]) {
+      c.push(a[i]);
+      i++;
+    } else {
+      c.push(b[j])
+      j++;
+    }
+  }
+
+  while (i < a.length) {
+    c.push(a[i]);
+    i++;
+  }
+
+  while (j < b.length) {
+    c.push(b[j]);
+    j++;
+  }
+
+  return c;
+
+}
+
+// TESTS
+let arr1 = [6, 3, 2, 4, 0, 5, 1]
+console.log(mergeSort(arr1));

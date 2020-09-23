a125d26bc47b1c74f110bffb61ebfd74a288a605
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-09-22 21:51:36 -0400

    Updated findNumbersWithEvenNumberOfDigits algo with second solution

--- leetcode/easy/arrays/findNumbersWithEvenNumbersOfDigits.js
+++ leetcode/easy/arrays/findNumbersWithEvenNumbersOfDigits.js
@@ -1,7 +1,21 @@
+// SOLUTION 1
 const findNumbers = nums => {
   let ret = 0;
   for (let i = 0; i < nums.length; ++i) {
     (nums[i].toString().length & 1) === 0 && ++ret;
+  }
+  return ret;
+};
+
+// SOLUTION 2
+const findNumbers = nums => {
+  let ret = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    let len = 0;
+    for (let val = nums[i]; val > 0; ++len) {
+      val = val / 10 >> 0;
+    }
+    (len & 1) === 0 && ++ret;
   }
   return ret;
 };

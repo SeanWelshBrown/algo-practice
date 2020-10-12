427d0b045f98d3364efe88db8786b14f2ef18808
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-10-12 15:53:16 -0400

    Added threeNumberSum algo

---
+++ AlgoExpert/medium/threeNumberSum.js
@@ -0,0 +1,24 @@
+function threeNumberSum(array, targetSum) {
+	array.sort((a, b) => a - b);
+  let result = [];
+	
+	for (let i = 0; i < array.length; i++) {
+		let left = i + 1;
+		let right = array.length - 1;
+		
+		while (left < right) {
+			let currentSum = array[i] + array[left] + array[right];
+			
+			if (currentSum < targetSum) {
+				left++;
+			} else if (currentSum > targetSum) {
+				right--;
+			} else if (currentSum === targetSum) {
+				result.push([array[i], array[left], array[right]]);
+				left++;
+			}
+		}
+	}
+	
+	return result;
+}
